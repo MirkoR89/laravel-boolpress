5f6c159f3e59144b8bf87258c1ee91ccaba8a97c
Mirko <mirko.rinelli@gmail.com>
2021-02-24 15:54:03 +0100

    Working on layout pages

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,15 @@
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="{{ route('home') }}">BOOLPRESS</a>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="{{ route('home') }}">Home</a>
+                    <a class="nav-link" href="{{ route('about') }}">About</a>
+                    <a class="nav-link" href="{{ route('contacts') }}">Articles</a>
+                    <a class="nav-link" href="{{ route('articles') }}">Articles</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
